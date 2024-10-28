d18db93154b831396b994a3412f8fd8438bf52fc
shibil-thora <shibilallakkat123@gmail.com>
2024-10-28 09:10:54 +0530

    updated port

--- main.py
+++ main.py
@@ -3,32 +3,20 @@ from models import TestModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI() 
-
-# Setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust as needed
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.post("/api")
+@app.post("/api/publish")
 async def create_item(userInput: TestModel):
     return {
         "data": f"{userInput.name}",
     }
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
-    except WebSocketDisconnect:
-        print("Client disconnected")
 
 if __name__ == "__main__":
     import uvicorn
