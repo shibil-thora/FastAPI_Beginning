e7617ed64c4217683bc883461be3c163d3c248f0
shibil-thora <shibilallakkat123@gmail.com>
2025-01-07 15:19:13 +0530

    added a simple websocket api that can echo request

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException
+from fastapi import FastAPI, WebSocket, Request
 from models import TestModel 
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware 
+import time 
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI() 
 app.add_middleware(
@@ -9,7 +13,23 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) 
+
+templates = Jinja2Templates(directory='templates')
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket): 
+    print(websocket)
+    await websocket.accept() 
+    while True: 
+        data = await websocket.receive_text() 
+        print(data)
+        await websocket.send_text(data)
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_index(request: Request): 
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/api/schedule")
